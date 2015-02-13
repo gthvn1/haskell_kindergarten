07d564e4e11e2e7a67917d5f64f9d80a1942a3de
Guillaume Thouvenin <thouveng@gmail.com>
2015-02-13 19:46:22 +0100

    added some accessor functions

--- realworldhaskell/chp5/SimpleJSON.hs
+++ realworldhaskell/chp5/SimpleJSON.hs
@@ -1,4 +1,11 @@
-
+module SimpleJSON ( JValue (..)
+                  , getString
+                  , getDouble
+                  , getBool
+                  , getObject
+                  , getArray
+                  , isNull
+                  ) where
 
 -- JSON have four basic types: Strings, number, booleans and special null
 -- It provides two compound types: Array, Object
@@ -16,3 +23,23 @@ data JValue = JString String
 getString :: JValue -> Maybe String
 getString (JString s) = Just s
 getString _ = Nothing
+
+getDouble :: JValue -> Maybe Double
+getDouble (JNumber n) = Just n
+getDouble _ = Nothing
+
+getBool :: JValue -> Maybe Bool
+getBool (JBool b) = Just b
+getBool _ = Nothing
+
+getObject :: JValue -> Maybe [(String, JValue)]
+getObject (JObject o) = Just o
+getObject _ = Nothing
+
+getArray :: JValue -> Maybe [JValue]
+getArray (JArray a) = Just a
+getArray _ = Nothing
+
+isNull :: JValue -> Bool
+isNull JNull = True
+isNull _ = False
