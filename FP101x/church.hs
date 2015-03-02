a8bb2f16e0e3d3054e9fd13b1de82e56745e58d2
Guillaume Thouvenin <thouveng@gmail.com>
2015-03-02 23:00:36 +0100

    added new definitions

--- FP101x/church.hs
+++ FP101x/church.hs
@@ -1,8 +1,31 @@
+type Church a = (a -> a) -> a -> a
+
+zero :: Church a
+zero  = \f z -> z
+
+one :: Church a
+one   = \f z -> f z
+
+two :: Church a
+two   = \f z -> (f.f) z
+
+three :: Church a
+three = \f z -> (f.f.f) z
+
+-- Example for church to int
+-- zero (+1) 0 => 0
+-- one (+1) 0  => 1
+-- two (+1) 0  => 2
+
+-- Example for church to stars
+-- zero ('*':) 0 => ""
+-- one ('*':) 0  => "*"
+-- two ('*':) 0  => "**"
+
 church :: Integer -> (a -> a) -> a -> a
 church 0 f x = x
 church n f x = f (church (n-1) f x)
 
--- Example for church to int
 c2i x = church x (+1) 0
 
 -- Example for church to stars
