e750862ae3f919ced405b824de79aff3cf303029
Guillaume Thouvenin <thouveng@gmail.com>
2015-03-02 23:34:01 +0100

    use foldr for church

--- FP101x/church.hs
+++ FP101x/church.hs
@@ -17,18 +17,17 @@ three = \f z -> (f.f.f) z
 -- one (+1) 0  => 1
 -- two (+1) 0  => 2
 
+-- A generic function
+c2i n = foldr (\x -> (+ 1)) 0 [1..n]
+
 -- Example for church to stars
 -- zero ('*':) 0 => ""
 -- one ('*':) 0  => "*"
 -- two ('*':) 0  => "**"
 
-church :: Integer -> (a -> a) -> a -> a
-church 0 f x = x
-church n f x = f (church (n-1) f x)
+-- A generic function
+c2s n = foldr (\x -> ('*':)) [] [1..n]
 
-c2i x = church x (+1) 0
-
--- Example for church to stars
--- 1 -> "*"
--- 5 -> "*****"
-c2s x = church x ('*':) []
+-- church :: Integer -> (a -> a) -> a -> a
+-- church 0 f x = x
+-- church n f x = f (church (n-1) f x)
