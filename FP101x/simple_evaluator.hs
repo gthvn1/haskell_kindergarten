9c2b39c4605a390a0dd1a1c2afa8e7e22570a08f
Guillaume Thouvenin <thouveng@gmail.com>
2015-03-08 14:44:13 +0100

    monads: continue

--- FP101x/simple_evaluator.hs
+++ FP101x/simple_evaluator.hs
@@ -14,6 +14,10 @@ eval :: Expr -> Int
 eval (Val n) = n
 eval (Div x y) = div (eval x) (eval y)
 
+-- ---------------------------------------------------------------------------
+-- The Monad world
+-- ---------------------------------------------------------------------------
+
 -- The problem here is that
 -- eval (Div (Val 10) (Val 0))
 -- => *** Exception: divide by zero
@@ -41,13 +45,22 @@ seqn Nothing _ = Nothing
 seqn _ Nothing = Nothing
 seqn (Just x) (Just y) = Just (x,y)
 
+-- So we take two maybe and return a tuple of maybe. eval can be redifined
+-- as follow:
+
+-- Note:
+--   we have: seqn (Just a) (Just b) -> Just (a,b)
+--  and we want to do: f (Just (a,b)) = f (a,b) with f = safeDiv
+-- So we need a function apply that will take a function, a Maybe a and
+-- will return a Maybe b.
+--
+apply :: (a -> Maybe b) -> Maybe a -> Maybe b
+apply _ Nothing = Nothing
+apply f (Just x) = f x
+
 eval'' (Val n) = Just n
 eval'' (Div x y) = apply f (seqn (eval'' x) (eval'' y))
                     where f (n, m) = safeDiv n m
-
-apply :: (a -> Maybe b) -> Maybe a -> Maybe b
-apply f Nothing = Nothing
-apply f (Just x) = f x
 
 -- eval'' (Div (Val 4) (Val 0))
 -- <=> eval'' (Div (Just 4) (Just 0))
@@ -63,3 +76,50 @@ apply f (Just x) = f x
 -- <=> f (4,2)
 -- <=> safeDiv 4 2
 -- <=> Just 2
+
+-- The problem here is if we have multiple parameter like eval (Op x y z),
+-- that will generates nested tuples.
+-- eval (Op x y z) = apply f (seqn (eval x) (seqn (eval y) (eval z)))
+--                 where ...
+
+-- To avoid that we can replace apply by putting f at the end:
+apply' :: Maybe a -> (a -> Maybe b) -> Maybe b
+apply' Nothing _ = Nothing
+apply' (Just x) f = f x
+
+-- we recognized the more generic
+-- (>>=) :: Monad m => m a -> (a -> m b) -> m b
+-- Nothing >>= _ = Nothing
+-- (Just x) >>= f = f x
+
+-- So we can rewrite eval as
+eval''' (Val n) = Just n
+eval''' (Div x y) = (eval''' x) `apply'` (\n ->
+                    (eval''' y) `apply'` (\m ->
+                    safeDiv n m))
+
+-- Again we see a pattern where the nested tuples are replaced by many
+-- apply' but Haskell provide a special do notation here so we can write:
+eval'''' (Val n) = Just n
+eval'''' (Div x y) = do n <- eval'''' x
+                        m <- eval'''' y
+                        safeDiv n m
+
+-- This notation is not specific to Maybe but can be generalized for all
+-- types that are instance of Monads. To be instance of monads, a type must
+-- support: return and >>=
+--
+-- That means:
+--  class Monad m where
+--    return :: a -> m a
+--    (>>=) :: m a -> (a -> m b) -> m b
+--
+--  instance Monad Maybe where
+--    -- return
+--    return x = Just x
+--    -- >>=
+--    Nothing >>= _ = Nothing
+--    (Just x) >> = f = f x
+
+-- Example for Maybe:
+--
