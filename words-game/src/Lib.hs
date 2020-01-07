721a6f1c3986dcee2268c1e707beeba1d1d8ae42
gthvn0 <gthvn0@gmail.com>
2020-01-07 21:07:58 +0100

    Add grid of flowers and function to display grid

--- words-game/src/Lib.hs
+++ words-game/src/Lib.hs
@@ -1,9 +1,40 @@
 module Lib
-    ( someFunc
-    , someString
+    ( grid
+    , flowerNames
+    , formatGrid
+    , outputGrid
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+outputGrid :: [String] -> IO ()
+outputGrid grid = putStrLn (formatGrid grid)
 
-someString = "someString"
+formatGrid :: [String] -> String
+formatGrid = unlines
+
+grid = [ "__D________R___"
+       , "__ANEMONE___O__"
+       , "__H__________S_"
+       , "__L________T__E"
+       , "__IRIS____O____"
+       , "__A_U____U__I__"
+       , "____G___R___I__"
+       , "_______N____M__"
+       , "______E_____S__"
+       , "_____SALIL__A__"
+       , "____O_______J__"
+       , "___L___________"
+       ]
+
+flowerNames = [ "ANEMONE"
+              , "DAHLIA"
+              , "GUI"
+              , "IRIS"
+              , "JASMIN"
+              , "LAVANDE"
+              , "LILAS"
+              , "PAVOT"
+              , "PIVOINE"
+              , "RENONCULE"
+              , "ROSE"
+              , "TOURNESOL"
+              ]
