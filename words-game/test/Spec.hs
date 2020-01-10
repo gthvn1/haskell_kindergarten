9707605d9e0ecbdbbd28c1709c1abcce39441d55
Guillaume <gthouvenin@kalray.eu>
2020-01-10 14:10:16 +0100

    Improve the tests

--- words-game/test/Spec.hs
+++ words-game/test/Spec.hs
@@ -4,10 +4,12 @@ import FlowerData
 
 main :: IO ()
 main = hspec $ do
-  describe "Test the Lib module" $ do
-    it "should able to print a Grid as a string" $ do
+  describe "Test unlines" $ do
+    it "should be able to print a Grid as a string" $ do
       unlines ["abc", "def"] `shouldBe` "abc\ndef\n"
 
-    it "should able to find a word in a grid" $ do
+  describe "Test findWord" $ do
+    it "should be able to find a word that is in a grid" $ do
       findWord flowerGrid "ANEMONE" `shouldBe` Just "ANEMONE"
+    it "should not be able to find a word that is not in a grid" $ do
       findWord flowerGrid "SOLEIL" `shouldBe` Nothing
