f798828817817743d10fb7a88c05acd3273bbdfe
Guillaume <gthouvenin@kalray.eu>
2020-01-13 12:27:32 +0100

    Add test for findWords

--- words-game/test/Spec.hs
+++ words-game/test/Spec.hs
@@ -13,3 +13,9 @@ main = hspec $ do
       findWord flowerGrid "ANEMONE" `shouldBe` Just "ANEMONE"
     it "should not be able to find a word that is not in a grid" $ do
       findWord flowerGrid "SOLEIL" `shouldBe` Nothing
+
+  describe "Test findWords" $ do
+    it "should be able to find all words in flowerNames" $ do
+      findWords flowerGrid flowerNames `shouldBe` flowerNames
+    it "should not be able to find wrong words " $ do
+      findWords flowerGrid ["GUI", "WRONG"] `shouldBe` ["GUI"]
